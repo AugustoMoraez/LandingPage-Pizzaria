83e482fb180269582f2e5c62ee2429b13b5bef5c
Augusto Moraes <supergamerbros2015@gmail.com>
2023-04-14 13:22:08 -0300

    if e elses principais feitos cart

--- src/hooks/reducers/reducerCart.ts
+++ src/hooks/reducers/reducerCart.ts
@@ -1,3 +1,4 @@
+import { Item } from "../../Componentes/itemCart";
 import { cartDB } from "../../data/cartDB";
 import { itemCartType } from "../../types/itemCart";
 import { reducerActionType } from "../../types/reducerActionType";
@@ -11,20 +12,41 @@ export const reducerCart = (state: itemCartType[], action: reducerActionType) =>
 
     switch (action.type) {
         case "Add_Cart":
-            if (action.payload.itemCart) {
-                const newState = [...state];
+            let newState = [...state];
+            if (action.payload.itemCart && state.length == 0) {
                 newState.push(action.payload.itemCart);
-                localStorage.setItem("cart", JSON.stringify(newState));
-                return newState
+                state = newState;
+                localStorage.setItem("cart", JSON.stringify(state));
+                return state;
+            } else {
+                const filter = newState.filter((item) => item.item.name == action.payload.itemCart.item.name);
+                const itemToAdd = filter.length == 0 ? null : filter[0];
+                const item: itemCartType = action.payload.itemCart
+
+                if (item.item.name === itemToAdd?.item.name) {
+                    
+                } else {
+                    newState.push(item);
+                    state = newState;
+                    localStorage.setItem("cart", JSON.stringify(state));
+                    return state;
+                }
+
+
+
             }
             break;
+
+
+
+
         case "Remove_Cart":
-            if (state.length === 1 ) {
+            if (state.length === 1) {
                 let newState = [...state]
                 state = newState.filter((item) => item.key !== action.payload.itemCart.key)
                 localStorage.setItem("cart", JSON.stringify([]));
                 return state
-                
+
             } else {
                 let newState = [...state]
                 state = newState.filter((item) => item.key !== action.payload.itemCart.key)
